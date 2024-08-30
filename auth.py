a1f74597d5c7a2f7c4cac3b62e8fea821d317d9c
Leoodaviid <david_ax7@hotmail.com>
2024-08-29 23:49:51 -0300

    feat(auth): integrate authentication and security features

---
+++ auth.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from urllib.parse import urlparse
+from flask_login import login_user, logout_user, current_user, login_required
+from models import db, User
+from forms import LoginForm, RegistrationForm
+from extensions import limiter  # Importe o limiter de extensions.py
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+@limiter.limit("10 per minute")
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('auth.login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or urlparse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('user_login.html', form=form) 
+
+@auth_bp.route('/logout', methods=['POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('auth.login'))
+    return render_template('user_create.html', form=form)
