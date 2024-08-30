072df5d7b8406a14af0ce0225b483cbc3d9d926c
Leoodaviid <david_ax7@hotmail.com>
2024-08-29 23:50:35 -0300

    refactor(config): improve configuration structure for different environments
    - Separated configuration into Development, Testing, and Production classes.
    - Enabled environment-based configuration loading.
    - Enhanced security with environment variables for database URIs.

--- config.py
+++ config.py
@@ -1,6 +1,23 @@
 import os
 
 class Config:
+    # Chave secreta usada para proteger dados de sessão, cookies e formulários CSRF
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///finance.db'
+
+    # Configuração comum a todos os ambientes
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
+    WTF_CSRF_ENABLED = False  # Desativa CSRF para testes
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///finance.db'
